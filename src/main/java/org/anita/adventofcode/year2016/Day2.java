b670badb2e47b844398411c1d321e7b5e5c5555b
Anita Fronczak <akarbow@gmail.com>
2019-10-23 19:31:14 +0200

    Day 2 part 2

--- src/main/java/org/anita/adventofcode/year2016/Day2.java
+++ src/main/java/org/anita/adventofcode/year2016/Day2.java
@@ -2,51 +2,87 @@ package org.anita.adventofcode.year2016;
 
 import org.anita.adventofcode.structures.Position2D;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public class Day2 {
-    public String lockpadNumber(List<String> steps) {
+
+    public String rectangularLockpadNumber(List<String> steps) {
+        return lockpadNumber(steps, new Position2D(1, 1), this::digit, this::withinBounds);
+    }
+
+    public String diamondLockpadNumber(List<String> steps) {
+        return lockpadNumber(steps, new Position2D(-2, 0), this::digit2, this::withinBounds2);
+    }
+
+    public String lockpadNumber(List<String> steps, Position2D start, Function<Position2D, Character> digit, Function<Position2D, Boolean> withinBounds) {
         String code = "";
-        Position2D current = new Position2D(1, 1);
+        Position2D current = start;
         for (String step : steps) {
             for (char move : step.toCharArray()) {
                 switch(move) {
                     case 'L': {
                         Position2D next = current.left();
-                        if (withinBonds(next)) {
+                        if (withinBounds.apply(next)) {
                             current = next;
                         }
                     } break;
                     case 'R': {
                         Position2D next = current.right();
-                        if (withinBonds(next)) {
+                        if (withinBounds.apply(next)) {
                             current = next;
                         }
                     } break;
                     case 'U': {
-                        Position2D next = current.up();
-                        if (withinBonds(next)) {
+                        Position2D next = current.down();
+                        if (withinBounds.apply(next)) {
                             current = next;
                         }
                     } break;
                     case 'D': {
-                        Position2D next = current.down();
-                        if (withinBonds(next)) {
+                        Position2D next = current.up();
+                        if (withinBounds.apply(next)) {
                             current = next;
                         }
                     } break;
                 }
             }
-            code += digit(current);
+            code += digit.apply(current);
         }
         return code;
     }
 
-    private int digit(Position2D position) {
-        return position.y * 3 + position.x + 1;
+    private Character digit(Position2D position) {
+        return Character.forDigit((2- position.y) * 3 + position.x + 1, 10);
     }
 
-    private boolean withinBonds(Position2D position) {
+    private boolean withinBounds(Position2D position) {
         return position.y >= 0 && position.y <=2 && position.x >= 0 && position.x <=2;
+    }
+
+    private Map<Position2D, Character> chars = new HashMap(){{
+        put(new Position2D(0, 2), '1');
+        put(new Position2D(-1, 1), '2');
+        put(new Position2D(0, 1), '3');
+        put(new Position2D(1, 1), '4');
+        put(new Position2D(-2, 0), '5');
+        put(new Position2D(-1, 0), '6');
+        put(new Position2D(0, 0), '7');
+        put(new Position2D(1, 0), '8');
+        put(new Position2D(2, 0), '9');
+        put(new Position2D(-1, -1), 'A');
+        put(new Position2D(0, -1), 'B');
+        put(new Position2D(1, -1), 'C');
+        put(new Position2D(0, -2), 'D');
+    }};
+
+    private Character digit2(Position2D position) {
+        return chars.get(position);
+    }
+
+    private boolean withinBounds2(Position2D position) {
+        return position.manhattanDistance(new Position2D(0,0)) <= 2;
     }
 }
--- src/test/java/org/anita/adventofcode/year2016/Day2Test.java
+++ src/test/java/org/anita/adventofcode/year2016/Day2Test.java
@@ -14,13 +14,27 @@ public class Day2Test {
     public void task1Example1() throws IOException {
         InputStream inputStream = getClass().getResourceAsStream("/2016_2_test.txt");
         List<String> steps = FileUtils.readElementsLineByLine(inputStream, s -> s);
-        Assert.assertEquals("1985", new Day2().lockpadNumber(steps));
+        Assert.assertEquals("1985", new Day2().rectangularLockpadNumber(steps));
     }
 
     @Test
     public void task1() throws IOException {
         InputStream inputStream = getClass().getResourceAsStream("/2016_2.txt");
         List<String> steps = FileUtils.readElementsLineByLine(inputStream, s -> s);
-        Assert.assertEquals("1985", new Day2().lockpadNumber(steps));
+        Assert.assertEquals("12578", new Day2().rectangularLockpadNumber(steps));
+    }
+
+    @Test
+    public void task2Example1() throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream("/2016_2_test.txt");
+        List<String> steps = FileUtils.readElementsLineByLine(inputStream, s -> s);
+        Assert.assertEquals("5DB3", new Day2().diamondLockpadNumber(steps));
+    }
+
+    @Test
+    public void task2() throws IOException {
+        InputStream inputStream = getClass().getResourceAsStream("/2016_2.txt");
+        List<String> steps = FileUtils.readElementsLineByLine(inputStream, s -> s);
+        Assert.assertEquals("516DD", new Day2().diamondLockpadNumber(steps));
     }
 }

94b104779fe278e521ce5cf2d0a1bf5a49e3a967
Anita Fronczak <akarbow@gmail.com>
2019-11-03 00:47:39 +0100

    Year 2018 day 21 java version

--- src/main/java/org/anita/adventofcode/year2018/Day21.java
+++ src/main/java/org/anita/adventofcode/year2018/Day21.java
@@ -1,63 +1,136 @@
 package org.anita.adventofcode.year2018;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Day21 {
 
-    int r0, r1, ip, r3, r4, r5;
+    private static int r0, r1, r3, r4, r5;
+    private static Set<Integer> seen = new HashSet<>();
+    private static int lastr4 = 0;
 
-    public void program() {
+    public static void main(String[] args) {
         r4 = 123;
-
-        do {
-            r4 = r4 & 456;
-            r4 = r4 == 72 ? 1 : 0;
-            //ip = r4 + ip;
-        } while (r4 == 0);
-        //ip = 0;
+        l1();
         r4 = 0;
-        r3 = r4 | 65536;
-        r4 = 10283511;
 
-        do {
+        while (true) {
+            l6:
+            r3 = r4 | 65536;
+            r4 = 10283511;
+
+            l8:
             r1 = r3 & 255;
             r4 = r4 + r1;
             r4 = r4 & 16777215;
             r4 = r4 * 65899;
             r4 = r4 & 16777215;
-
             r1 = 256 > r3 ? 1 : 0;
-        } while (r1 == 1);
+            while (r1 == 0) {
+                r1 = 0;
 
-        //ip = r1 + ip;
-        //ip = ip + 1;
-        //ip = 27;
+                l18:
+                do {
+                    r5 = r1 + 1;
+                    r5 = r5 * 256;
+                    r5 = r5 > r3 ? 1 : 0;
+                    r1 = r1 + 1;
+                } while (r5 == 0);
+                r1 = r1 - 1;
 
-        r1 = 0;
-        r5 = r1 + 1;
-        r5 = r5 * 256;
+                l26:
+                r3 = r1;
 
-        r5 = r5 > r3 ? 1 : 0;
+                l8:
+                r1 = r3 & 255;
+                r4 = r4 + r1;
+                r4 = r4 & 16777215;
+                r4 = r4 * 65899;
+                r4 = r4 & 16777215;
+                r1 = 256 > r3 ? 1 : 0;
 
-        if (r5 == 1) {
+            }
 
-        } else {
-
+            l28:
+            if (lastr4 == 0) {
+                System.out.println("Part1: " + r4);
+            }
+            if (!seen.contains(r4)) {
+                seen.add(r4);
+                lastr4 = r4;
+            } else {
+                System.out.println("Part2: " + lastr4);
+                return;
+            }
+            r1 = r4 == r0 ? 1 : 0;
         }
 
-
-
-        ip = r5 + ip;
-        ip = ip + 1;
-        ip = 25;
-        r1 = r1 + 1;
-        ip = 17;
-        r3 = r1;
-        ip = 7;
-        r1 = r4 == r0 ? 1 : 0;
-        ip = r1 + ip;
-        ip = 5;
+        //f();
     }
 
-    private void l1() {
+    private static void f() {
+        r4 = 123;
+        l1();
+        r4 = 0;
+        l6();
+    }
 
+    private static void l1() {
+        do {
+            r4 = r4 & 456;
+            r4 = r4 == 72 ? 1 : 0;
+        } while (r4 == 0);
+    }
+
+    private static void l6() {
+        r3 = r4 | 65536;
+        r4 = 10283511;
+        l8();
+    }
+
+    private static void l8() {
+        r1 = r3 & 255;
+        r4 = r4 + r1;
+        r4 = r4 & 16777215;
+        r4 = r4 * 65899;
+        r4 = r4 & 16777215;
+        r1 = 256 > r3 ? 1 : 0;
+        if (r1 == 1) {
+            l28();
+        } else {
+            r1 = 0;
+            l18();
+        }
+    }
+
+    private static void l28() {
+        if (lastr4 == 0) {
+            System.out.println("Part1: " + r4);
+        }
+        if (!seen.contains(r4)) {
+            seen.add(r4);
+            lastr4 = r4;
+        } else {
+            System.out.println("Part2: " + lastr4);
+            return;
+        }
+        r1 = r4 == r0 ? 1 : 0;
+        if (r1 == 0) l6();
+    }
+
+    private static void l18() {
+        do {
+            r5 = r1 + 1;
+            r5 = r5 * 256;
+            r5 = r5 > r3 ? 1 : 0;
+            r1 = r1 + 1;
+        } while (r5 == 0);
+        r1 = r1 - 1;
+        l26();
+    }
+
+    private static void l26() {
+        r3 = r1;
+        l8();
     }
 }

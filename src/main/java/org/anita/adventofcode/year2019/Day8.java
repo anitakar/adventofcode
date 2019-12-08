80526693ad8905d600556f74d02e5696abb1cba4
Anita Fronczak <akarbow@gmail.com>
2019-12-08 16:19:24 +0100

    Year 2019 Day 8 Part 1 + 2

---
+++ src/main/java/org/anita/adventofcode/year2019/Day8.java
@@ -0,0 +1,62 @@
+package org.anita.adventofcode.year2019;
+
+import java.util.Arrays;
+
+public class Day8 {
+
+    public int task1(String image, int width, int height) {
+        int layerSize = width * height;
+        int minimalNumberOfZeros = Integer.MAX_VALUE;
+        int minimalLayerControlSum = 0;
+        int numberOfZeros = Integer.MAX_VALUE;
+        int numberOfOnes = Integer.MAX_VALUE;
+        int numberOfTwos = Integer.MAX_VALUE;
+        char[] chars = image.toCharArray();
+        for (int i = 0; i < chars.length; ++i) {
+            if (i % layerSize == 0) {
+                if (numberOfZeros < minimalNumberOfZeros) {
+                    minimalNumberOfZeros = numberOfZeros;
+                    minimalLayerControlSum = numberOfOnes * numberOfTwos;
+                }
+                numberOfZeros = 0;
+                numberOfOnes = 0;
+                numberOfTwos = 0;
+            }
+            if (chars[i] == '0') {
+                numberOfZeros++;
+            } else if (chars[i] == '1') {
+                numberOfOnes++;
+            } else if (chars[i] == '2') {
+                numberOfTwos++;
+            }
+        }
+        return minimalLayerControlSum;
+    }
+
+    public void task2(String image, int width, int height) {
+        int[] decodedImage = new int[height * width];
+        Arrays.fill(decodedImage, 2);
+        int layerSize = width * height;
+        char[] chars = image.toCharArray();
+        for (int i = 0; i < chars.length; ++i) {
+            int pos = i % layerSize;
+            if (decodedImage[pos] == 2) {
+                decodedImage[pos] = chars[i] - '0';
+            }
+        }
+
+        for (int h = 0; h < height; ++h) {
+            for (int w = 0; w < width; ++w) {
+                int pixel = decodedImage[h * width + w];
+                if (pixel == 2) {
+                    System.out.print(" ");
+                } else if (pixel == 1) {
+                    System.out.print("1");
+                } else if (pixel == 0) {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/test/java/org/anita/adventofcode/year2019/Day8Test.java
@@ -0,0 +1,32 @@
+package org.anita.adventofcode.year2019;
+
+import org.anita.adventofcode.util.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Day8Test {
+
+    @Test
+    public void task1() throws IOException {
+        InputStream stream = getClass().getResourceAsStream("/2019_8.txt");
+        String image = FileUtils.readStringsLineByLine(stream).get(0);
+
+        Assert.assertEquals(0, new Day8().task1(image, 25, 6));
+    }
+
+    @Test
+    public void task2Test() throws IOException {
+        new Day8().task2("0222112222120000", 2, 2);
+    }
+
+    @Test
+    public void task2() throws IOException {
+        InputStream stream = getClass().getResourceAsStream("/2019_8.txt");
+        String image = FileUtils.readStringsLineByLine(stream).get(0);
+
+        new Day8().task2(image, 25, 6);
+    }
+}

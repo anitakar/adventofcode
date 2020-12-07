bc4ea05a6f717c39fad5e973af16f3e0e2681ef1
Anita Fronczak <akarbow@gmail.com>
2020-12-07 15:19:34 +0100

    Year 2020 Day 3

--- src/main/java/org/anita/adventofcode/year2020/Day3.java
+++ src/main/java/org/anita/adventofcode/year2020/Day3.java
@@ -1,2 +1,58 @@
-package org.anita.adventofcode.year2020;public class Day3 {
+package org.anita.adventofcode.year2020;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Day3 {
+
+    public int task1(Map map, int stepsRight, int stepsDown) {
+        int treesCount = 0;
+        int y = 0;
+        int x = 0;
+        do {
+            y += stepsDown;
+            x += stepsRight;
+            if (y < map.height() && map.read(x, y) == '#') {
+                treesCount += 1;
+            }
+        } while (y < map.height() - 1);
+        return treesCount;
+    }
+
+    public static class Map {
+        private char[][] pattern;
+
+        public Map(char[][] pattern) {
+            this.pattern = pattern;
+        }
+
+        public int height() {
+            return pattern.length;
+        }
+
+        public int recurrenceWidth() {
+            return pattern[0].length;
+        }
+
+        public char read(int x, int y) {
+            return pattern[y][x % recurrenceWidth()];
+        }
+    }
+
+    public static class MapReader {
+        private List<char[]> lines = new LinkedList<>();
+
+        public boolean readLine(String line) {
+            lines.add(line.toCharArray());
+            return true;
+        }
+
+        public Map createMap() {
+            char[][] result = new char[lines.size()][lines.get(0).length];
+            for (int i = 0; i < lines.size(); ++i) {
+                result[i] = lines.get(i);
+            }
+            return new Map(result);
+        }
+    }
 }
--- src/test/java/org/anita/adventofcode/year2020/Day3Test.java
+++ src/test/java/org/anita/adventofcode/year2020/Day3Test.java
@@ -1,3 +1,46 @@
 package org.anita.adventofcode.year2020;
 
+import org.anita.adventofcode.util.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Day3Test {
+
+    @Test
+    public void task1Test() throws IOException {
+        Day3.MapReader mapReader = new Day3.MapReader();
+
+        InputStream inputStream = getClass().getResourceAsStream("/2020_3_test.txt");
+        FileUtils.readElementsLineByLine(inputStream, mapReader::readLine);
+        Assert.assertEquals(7, new Day3().task1(mapReader.createMap(), 3, 1));
+    }
+
+    @Test
+    public void task1() throws IOException {
+        Day3.MapReader mapReader = new Day3.MapReader();
+
+        InputStream inputStream = getClass().getResourceAsStream("/2020_3.txt");
+        FileUtils.readElementsLineByLine(inputStream, mapReader::readLine);
+        Assert.assertEquals(214, new Day3().task1(mapReader.createMap(), 3, 1));
+    }
+
+    @Test
+    public void task2() throws IOException {
+        Day3.MapReader mapReader = new Day3.MapReader();
+
+        InputStream inputStream = getClass().getResourceAsStream("/2020_3.txt");
+        FileUtils.readElementsLineByLine(inputStream, mapReader::readLine);
+        Day3.Map map = mapReader.createMap();
+
+        long trees1 = new Day3().task1(map, 1, 1);
+        long trees2 = new Day3().task1(map, 3, 1);
+        long trees3 = new Day3().task1(map, 5, 1);
+        long trees4 = new Day3().task1(map, 7, 1);
+        long trees5 = new Day3().task1(map, 1, 2);
+
+        Assert.assertEquals(8336352024L, trees1 * trees2 * trees3 * trees4 * trees5);
+    }
+}
